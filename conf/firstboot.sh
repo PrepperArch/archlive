e8d69bbf0f5d42b106162fecb5e08ab07ba2d153
Henrik Andersson <henrik.4e@gmail.com>
2023-11-06 23:02:57 +0100

    Add init of pacman keystore

--- conf/firstboot.sh
+++ conf/firstboot.sh
@@ -3,3 +3,7 @@
 # Add prepper user
 useradd -s /usr/bin/bash -m -U -G wheel,network -k /etc/skel prepper
 echo 'prepper:prepper' | chpasswd --crypt-method SHA512
+
+# setup pacman
+pacman-key --init
+pacman-key --populate
